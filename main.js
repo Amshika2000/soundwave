1a5f5f038cbc93a2e49bbb76019317673a5fe3c1
Amshika2000 <amshika2000@gmail.com>
2021-09-13 13:27:39 +0530

    my 1st commit

---
+++ main.js
@@ -0,0 +1,203 @@
+//AR rahman Songs
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+
+  SC.stream('/tracks/347843083',function(sound){
+
+    $('#start1').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+           $('#stop2').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+            });
+
+
+   });
+
+  });
+
+  //Aniruth Songs
+
+  SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+  $(document).ready(function() {
+
+    SC.stream('/tracks/577816632',function(sound){
+
+      $('#start2').click(function(e) {
+               e.preventDefault();
+               sound.start();
+             });
+             $('#stop2').click(function(e) {
+              e.preventDefault();
+              sound.stop();
+              });
+
+
+     });
+
+    });
+
+  //sidth sriram Songs
+    SC.initialize({
+      client_id: '340f063c670272fac27cfa67bffcafc4'
+    });
+    $(document).ready(function() {
+
+      SC.stream('/tracks/749822650',function(sound){
+
+        $('#start3').click(function(e) {
+                 e.preventDefault();
+                 sound.start();
+               });
+               $('#stop3').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+                });
+
+
+       });
+
+      });
+
+//GV Songs
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+
+  SC.stream('/tracks/107780850',function(sound){
+
+    $('#start4').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+           $('#stop4').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+            });
+
+
+   });
+
+  });
+
+    //u1 Songs
+    SC.initialize({
+      client_id: '340f063c670272fac27cfa67bffcafc4'
+    });
+    $(document).ready(function() {
+
+      SC.stream('/tracks/120280943',function(sound){
+
+        $('#start5').click(function(e) {
+                 e.preventDefault();
+                 sound.start();
+               });
+               $('#stop5').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+                });
+
+
+       });
+
+      });
+
+      //kerthik Songs
+
+
+      SC.initialize({
+        client_id: '340f063c670272fac27cfa67bffcafc4'
+      });
+      $(document).ready(function() {
+
+        SC.stream('/tracks/22723601',function(sound){
+
+          $('#start6').click(function(e) {
+                   e.preventDefault();
+                   sound.start();
+                 });
+                 $('#stop6').click(function(e) {
+                  e.preventDefault();
+                  sound.stop();
+                  });
+
+
+         });
+
+        });
+
+    //Imman Songs
+
+    SC.initialize({
+      client_id: '340f063c670272fac27cfa67bffcafc4'
+    });
+    $(document).ready(function() {
+
+      SC.stream('/tracks/1046855623',function(sound){
+
+        $('#start7').click(function(e) {
+                 e.preventDefault();
+                 sound.start();
+               });
+               $('#stop7').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+                });
+
+
+       });
+
+      });
+
+//priyanka song
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+
+  SC.stream('/tracks/162364366',function(sound){
+
+    $('#start8').click(function(e) {
+             e.preventDefault();
+             sound.start();
+           });
+           $('#stop8').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+            });
+
+
+   });
+
+  });
+ //krish Songs
+
+ SC.initialize({
+   client_id: '340f063c670272fac27cfa67bffcafc4'
+ });
+ $(document).ready(function() {
+
+   SC.stream('/tracks/28421167',function(sound){
+
+     $('#start9').click(function(e) {
+              e.preventDefault();
+              sound.start();
+            });
+            $('#stop9').click(function(e) {
+             e.preventDefault();
+             sound.stop();
+             });
+
+
+    });
+
+   });
